4d262b08b2ed57f2152f0c854a4cae99b3f380ff
“Varunps91” <Starklabsindia@gmail.com>
2022-06-21 14:09:17 +0530

    Revert "update auth.js"
    This reverts commit 112b1c8165b01a65524b2c9d10493a3ba432eb74.

--- api/auth.js
+++ api/auth.js
@@ -99,40 +99,6 @@ router.get("/auth/user", verifyToken, async (req, res) => {
   }
 });
 
-router.get("/auth/user/:guid", verifyToken, async (req, res) => {
-  let reqData = req.params.guid;
-  try {
-    // let foundUser = await User.findOne({ _id: req.decoded._id }).populate(
-    //   "address"
-    // );
-    let foundUser = {};
-    MongoClient.connect(db_url, function (err, client) {
-      if (err) console.log('err', err);
-      const db = client.db("admission");
-      const collection = db.collection('User');
-
-      collection.findOne({ guid: reqData }, (err, result) => {
-        if (err) console.log('err', err);
-        console.log('result', result);
-        foundUser = result;
-        if (foundUser) {
-          res.json({
-            success: true,
-            result: foundUser
-          });
-        }
-      })
-    });
-
-
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
-  }
-});
-
 /* Update a profile */
 router.put("/auth/user", verifyToken, async (req, res) => {
   try {
@@ -164,61 +130,6 @@ router.put("/auth/user", verifyToken, async (req, res) => {
             const collection = db.collection('User');
             const body = { $set: { name: foundUser.name, email: foundUser.email, password: foundUser.password, contact: foundUser.contact } }
             collection.updateOne({ _id: req.decoded._id }, body, (err, result) => {
-              if (err) console.log('err', err);
-              console.log('result', result);
-              //foundUser = result
-            })
-          });
-
-
-          res.json({
-            success: true,
-            message: "Successfully updated"
-          });
-        }
-      })
-    });
-
-
-  } catch (err) {
-    res.status(500).json({
-      success: false,
-      message: err.message
-    });
-  }
-});
-
-router.put("/auth/user/:guid", verifyToken, async (req, res) => {
-  let reqData = req.params.guid;
-  try {
-    // let foundUser = await User.findOne({ _id: req.decoded._id });
-    let foundUser = {}
-    MongoClient.connect(db_url, function (err, client) {
-      if (err) console.log('err', err);
-      const db = client.db("admission");
-      const collection = db.collection('User');
-
-      collection.findOne({ guid: reqData }, async (err, result) => {
-        if (err) console.log('err', err);
-        console.log('result', result);
-        foundUser = result
-        if (foundUser) {
-          if (req.body.name) foundUser.name = req.body.name;
-          if (req.body.email) foundUser.email = req.body.email;
-          if (req.body.contact) foundUser.contact = req.body.contact;
-          if (req.body.password) {
-            const salt = await bcrypt.genSalt(10);
-            const hashedPassword = await bcrypt.hash(req.body.password, salt);
-            foundUser.password = hashedPassword;
-          }
-
-          //   await foundUser.save();
-          MongoClient.connect(db_url, function (err, client) {
-            if (err) console.log('err', err);
-            const db = client.db("admission");
-            const collection = db.collection('User');
-            const body = { $set: { name: foundUser.name, email: foundUser.email, password: foundUser.password, contact: foundUser.contact } }
-            collection.updateOne({ guid: reqData }, body, (err, result) => {
               if (err) console.log('err', err);
               console.log('result', result);
               //foundUser = result
