05e946e3768997c1b79ce9b192e95f1aa92566f3
varunsinghDG <85149973+varunsinghDG@users.noreply.github.com>
2022-08-24 14:13:04 +0530

    cors issue

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.options('*', cors());
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'example.com');
+  res.header('Access-Control-Allow-Origin', 'https://admi-frontend.vercel.app/');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
